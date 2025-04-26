66c058533a46187673f1aa49bbfeae7df07f5f81
maria-maqbool <mariamaqbool35@gmail.com>
2025-04-26 23:46:49 +0500

    add Product details page

--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -10,6 +10,7 @@ const ProductPrice = ({value, className}: {value: number; className?: string}) =
     return ( 
         <p className={cn('text-2xl', className)}>
             <span className="text-xs align-super">$</span>
+            {intValue}
             <span className="text-xs align-super">.{floatValue}</span>
         </p>
      );
